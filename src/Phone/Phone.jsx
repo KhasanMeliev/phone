b0ea1d4b9c030c777a21c1d2b1eb9f334e615138
melievkh <khushnudmeliev21@gmail.com>
2022-09-26 21:32:39 +0500

    phone

---
+++ src/Phone/Phone.jsx
@@ -0,0 +1,73 @@
+import { Burger, Grid, Text } from "@mantine/core";
+import React, { useState } from "react";
+import {
+  Box,
+  Header,
+  Line,
+  Main,
+  PhoneBox,
+  PhoneScreen,
+  Round,
+  Wrapper,
+} from "./Phone.style";
+import bugatti from "../assets/images/bugatti.jfif";
+import lamborghini from "../assets/images/lamborghini.jfif";
+import rollsroyce from "../assets/images/rollsroyce.jfif";
+import avtr from "../assets/images/avtr.webp";
+import lotus from "../assets/images/lotus.jpg";
+import tesla from "../assets/images/tesla.jpg";
+import mustang from "../assets/images/mustang.jpg";
+
+const Phone = () => {
+  const [opened, setOpened] = useState(false);
+  const title = opened ? "Close navigation" : "Open navigation";
+  return (
+    <Wrapper>
+      <PhoneBox>
+        <Line></Line>
+        <PhoneScreen>
+          <Header>
+            <Text color="white" size={25}>
+              Alex
+            </Text>
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((o) => !o)}
+              title={title}
+              color="white"
+            />
+          </Header>
+          <Main>
+            <h1>Website</h1>
+            <p>This website about cars.</p>
+
+            <Box>
+              <img src={bugatti} alt="bugatti" />
+            </Box>
+            <Box>
+              <img src={lamborghini} alt="lamborghini" />
+            </Box>
+            <Box>
+              <img src={rollsroyce} alt="rollsroyce" />
+            </Box>
+            <Box>
+              <img src={avtr} alt="avtr" />
+            </Box>
+            <Box>
+              <img src={lotus} alt="lotus" />
+            </Box>
+            <Box>
+              <img src={tesla} alt="tesla" />
+            </Box>
+            <Box>
+              <img src={mustang} alt="mustang" />
+            </Box>
+          </Main>
+        </PhoneScreen>
+        <Round></Round>
+      </PhoneBox>
+    </Wrapper>
+  );
+};
+
+export default Phone;
---
+++ src/Phone/Phone.style.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #2a2f3d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const PhoneBox = styled.div`
+  width: 270px;
+  height: 460px;
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px 0px;
+  box-sizing: border-box;
+  align-items: center;
+  gap: 20px;
+`;
+export const Line = styled.div`
+  width: 40%;
+  height: 15px;
+  background-color: #2a2f3d;
+`;
+export const PhoneScreen = styled.div`
+  width: 85%;
+  height: 80%;
+  background-color: #2a2f3d;
+`;
+export const Round = styled.div`
+  width: 60px;
+  height: 60px;
+  background-color: #2a2f3d;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  height: 45px;
+  background-color: #d00df4;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
+export const Main = styled.div`
+  width: 100%;
+  height: 225px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 0px;
+  overflow: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  h1 {
+    color: white;
+    margin: 0;
+  }
+  p {
+    color: white;
+    margin: 0;
+  }
+`;
+
+export const Box = styled.div`
+  width: 80%;
+  height: 150px;
+  background-color: white;
+  border-radius: 4px;
+  box-sizing: border-box;
+  img {
+    width: 183px;
+    height: 150px;
+  }
+`;
